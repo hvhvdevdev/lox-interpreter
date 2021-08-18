bbd7a7e2eb106c0a984a703e470193cb41ca9a81
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-08-18 22:05:29 +0700

    main

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
-function main(): void {
-    console.log("Hello, world!")
+class Lox {
+    public static main(args: string[]): void {
+        if (args.length > 1) {
+            console.log("Usage: tslox [script]")
+            throw new Error("64")
+        } else if (args.length == 1) {
+            this.runFile(args[0])
+        } else {
+            this.runPrompt()
+        }
+    }
 }
 
-window.onload = (_e) => main()
+window.onload = (_e) => Lox.main([])
